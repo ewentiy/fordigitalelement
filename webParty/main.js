2ce6e2b04930ad2801880bb4b966a635b10b5a06
ewentiy <ashepkow@gmail.com>
2026-01-29 14:04:56 +0500

    2

---
+++ webParty/main.js
@@ -0,0 +1,11 @@
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    // Анимация иконки "бургер"
+    burger.classList.toggle('toggle');
+});
+
+
+
